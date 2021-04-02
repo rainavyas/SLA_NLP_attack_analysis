e2277ba16d689401652357fd957bbe4a82e6254d
Vyas Raina <sayvaniar@gmail.com>
2021-04-02 22:48:36 +0100

    y log

--- attn_layer_analysis.py
+++ attn_layer_analysis.py
@@ -73,7 +73,7 @@ def plot_decomposition(ranks, cos_dists_auth, cos_dists_attack, filename, rank_l
     plt.plot(ranks, cos_dists_attack, label="Attacked")
     plt.xlabel("Eigenvalue Rank")
     plt.ylabel("Average Absolute Cosine Distance")
-    plt.xscale('log')
+    plt.yscale('log')
     plt.legend()
     plt.savefig(filename)
     plt.clf()
