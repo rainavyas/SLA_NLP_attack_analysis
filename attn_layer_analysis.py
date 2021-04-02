814bf7abc8aece29b9424a42ee284025dc399519
Vyas Raina <sayvaniar@gmail.com>
2021-04-02 18:34:44 +0100

    multihead output analysis plots

---
+++ attn_layer_analysis.py
@@ -0,0 +1,6 @@
+'''
+PCA analysis of output of post-encoder mutlihead attention layer
+with and without appended adversarial phrases.
+
+The PCA basis vectors are found using authentic samples only
+'''
